e50f0dca424e14893cc5d6ecf1000d5aa7f40bee
Bruno Hirata <brn.hirata@gmail.com>
2025-12-02 19:33:18 +0000

    Add origin

--- 02-12-2025/backend/src/server.js
+++ 02-12-2025/backend/src/server.js
@@ -7,7 +7,20 @@ import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://techof-2025-frontend.onrender.com"
+]
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not Allowed by CORS'))
+  }
+}));
 app.use(express.json());
 
 app.use("/students", studentRoutes);
